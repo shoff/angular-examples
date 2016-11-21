75caecf64557a5c886e6cfd4814f87a707ee2dff
Durga Prasad Narikalapa <Durga@Durgas-MacBook-Pro.local>
2016-11-21 18:10:52 +0530

    angular start up

--- 01-startup/js/app.js
+++ 01-startup/js/app.js
@@ -1,3 +1,15 @@
-(function () {
-    angular.module('myApp', [ /* your modules here */ ]);
-})();
+angular.module('myApp', [ /* your modules here */ ])
+.controller("HelloCtrl", function($scope) {
+    $scope.message = "Hello World"
+    $scope.name = "Awesome"
+
+    $scope.user = {
+        firstName : "Hello Mr."
+    }
+
+    $scope.logins = [];
+    $scope.login = function () {
+        $scope.logins.push($scope.user.firstName + " was logged in.");
+    };
+
+});
